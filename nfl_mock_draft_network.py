af824bd438b62251a5858ca88770300d6dddda9a
Aaron Smith <aaroncolesmith@gmail.com>
2021-02-21 11:50:29 -0800

    first commit

--- nfl_mock_draft_network.py
+++ nfl_mock_draft_network.py
@@ -30,17 +30,15 @@ d3=pd.merge(d1.groupby(['player']).agg({'pick':'mean'}).reset_index(), d2.groupb
 d3['chg'] = d3['pick_0'] - d3['pick_1']
 d3['pct_chg']=d3['chg'] / d3['pick_1']
 d3=d3.sort_values('chg',ascending=True)
-st.write(d3.head(7))
-st.write(d3.tail(7))
 
 col1, col2 = st.beta_columns(2)
 col1.success("### Players Rising :fire:")
-for i, r in d3.head(5).iterrows():
+for i, r in d3.loc[d3.chg.notnull()].head(5).iterrows():
     col1.write(r['player']+' - trending ' + str(round(abs(r['chg']),2)) + ' picks earlier')
 
 
 col2.warning("### Players Falling 🧊")
-for i, r in d3.tail(5).iterrows():
+for i, r in d3.loc[d3.chg.notnull()].tail(5).iterrows():
     col2.write(r['player'] + ' - trending ' + str(round(r['chg'],2)) +' picks later')
 
 
