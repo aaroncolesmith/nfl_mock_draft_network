88bb6e3792a537e78f28b3598c10555be4beaa28
Aaron Smith <aaroncolesmith@gmail.com>
2021-02-25 14:15:42 -0800

    first commit

--- nfl_mock_draft_network.py
+++ nfl_mock_draft_network.py
@@ -78,6 +78,8 @@ nt = Network(directed=False,
 
 nt.force_atlas_2based(damping=2)
 
+icon = st.checkbox('Show icons (slows it down a bit)')
+
 for i, r in d.iterrows():
     nt.add_node(r['player'],
                 size=r['times_picked'],
@@ -86,8 +88,9 @@ for i, r in d.iterrows():
     nt.add_node(r['team_pick'],
                 size=r['team_times_picked'],
                 color={'background':'#FA70C8','border':'#EC0498'},
-                shape='image',
-                image =r['team_img'],
+                if icon:
+                    shape='image',
+                    image =r['team_img'],
                 title='<b>' +r['team_pick'] + ' - ' +str(r['team_times_picked'])+'  total picks</b> <br> ' + d.loc[d.team_pick == r['team_pick']].groupby('team_pick').apply(lambda x: ', <br>'.join(x.player_pick_str)).to_frame('cnt').reset_index()['cnt'].item())
     nt.add_edge(r['player'],
                 r['team_pick'],
