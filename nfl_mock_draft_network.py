f02adbda86abbaf55ea32bb7aa75ba3f9323058c
Aaron Smith <aaroncolesmith@gmail.com>
2021-03-08 08:13:47 -0800

    first commit

--- nfl_mock_draft_network.py
+++ nfl_mock_draft_network.py
@@ -193,11 +193,11 @@ col1, col2, col3 = st.beta_columns((2,4,2))
 #     col3.write('Pick ' + str(r['pick']) + ' - ' + r['player'])
 #     col3.write('')
 #
-# df_table=df.loc[df['source_date'] == draft].sort_values('pick',ascending=True).reset_index(drop=True)
-# df_table['team'] = ["<img src='" + r.team_img
-#     + f"""' style='display:block;margin-left:auto;margin-right:auto;width:32px;border:0;'><div style='text-align:center'>"""
-#     # + "<br>".join(r.team.split()) + "</div>"
-#     for ir, r in df_table.iterrows()]
+df_table=df.loc[df['source_date'] == draft].sort_values('pick',ascending=True).reset_index(drop=True)
+df_table['team'] = ["<img src='" + r.team_img
+    + f"""' style='display:block;margin-left:auto;margin-right:auto;width:32px;border:0;'><div style='text-align:center'>"""
+    # + "<br>".join(r.team.split()) + "</div>"
+    for ir, r in df_table.iterrows()]
 
 col2.write(df_table[['pick','team','player']].to_html(index=False,escape=False), unsafe_allow_html=True)
 
