1fd8a2eee7560a5e3b3d6e99fe99583baf30be2b
Aaron Smith <aaroncolesmith@gmail.com>
2021-03-08 07:15:26 -0800

    first commit

--- nfl_mock_draft_network.py
+++ nfl_mock_draft_network.py
@@ -181,7 +181,7 @@ draft = st.selectbox('Pick a draft to view:',df['source_date'].unique())
 st.write(df.loc[df['source_date'] == draft].sort_values('pick',ascending=True).reset_index(drop=True))
 
 
-col1, col2 = st.beta_columns(2)
+col1, col2 = st.beta_columns((1,8))
 for i,r in df.loc[df['source_date'] == draft].sort_values('pick',ascending=True).reset_index(drop=True).iterrows():
     col1.image(r['team_img'], width=72)
     col2.write('Pick ' + str(r['pick']) + ' - ' + r['player'])
