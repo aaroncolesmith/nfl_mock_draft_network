d9c46365fb66b692a23b3e0606307e63a4f375e8
Aaron Smith <32134610+aaroncolesmith@users.noreply.github.com>
2022-01-15 07:05:28 -0800

    s

--- nfl_mock_draft_network.py
+++ nfl_mock_draft_network.py
@@ -180,7 +180,7 @@ def app():
                 # width="940px",
                 heading='')
 
-    nt.force_atlas_2based(damping=0, spring_length=100, overlap=1)
+    nt.force_atlas_2based(damping=0, spring_length=100)
 
     # icon2 = st.checkbox('Show icons (slows it down a bit)',key='icon2')
 
